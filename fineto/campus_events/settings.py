dbba3da7006b7d82a3df49fcb4bd06115c83eb23
Sadem Hussen <154666429+crackasuper@users.noreply.github.com>
2026-01-30 21:02:43 +0000

    preparing catches in the project setting

--- fineto/campus_events/settings.py
+++ fineto/campus_events/settings.py
@@ -83,6 +83,18 @@ DATABASES = {
     }
 }
 
+#defining catches for requests
+
+CATCHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+    }
+}
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
