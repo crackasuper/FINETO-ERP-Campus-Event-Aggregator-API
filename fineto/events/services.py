0c2bcfde21621d61997c858adbfc0ad1fd674059
Sadem Hussen <154666429+crackasuper@users.noreply.github.com>
2026-01-30 20:53:48 +0000

    defining fetch function that return data from source one

--- fineto/events/services.py
+++ fineto/events/services.py
@@ -6,13 +6,37 @@ import requests
 def normalize_event(title, location, start_time, source):
     return {
         "title": title,
+
         "location": location,
+
         "start_time": start_time,
+
         "spource": source,
 
     }
 
 
 #defining two external mock APIs
-SOURCE_A_API = "https://api.sourcea.com/events"
-SOURCE_B_API = "https://api.sourceb.com/campus_events"
+SOURCE_ONE_API = "https://api.sourcea.com/events"
+
+SOURCE_TWO_API = "https://api.sourceb.com/campus_events"
+
+
+#defining functions that fetches and normalizes events from first sources
+def fetch_events_from_source_one():
+    response = requests.get(SOURCE_ONE_API, timeout=5) #5 seconds timeout
+
+    data = response.json()
+
+    return [
+        {
+            "title": e.get["name"],
+            "location": e.get["venue"],
+            "start_time": e.get["date"],
+            "source": "Source One",
+        }
+        for e in data
+    ]
+
+#defining functions that fetches and normalizes events from second sources
+            
